56331a197be5b8585bba82ff930414f366506452
PamelaKelly <pamelakelly7@gmail.com>
2017-11-06 22:08:41 +0000

    simplified match preferences app to remove bugs - now basic functionality working

--- MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MainActivity.java
+++ MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MainActivity.java
@@ -1,13 +1,34 @@
 package com.matchmaker.matchmaker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+    // public static final String EXTRA_MESSAGE = "com.matchmaker.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the send button
+     * Note the necessary requirements in order for this method
+     * to be recognised by the onclick property in the activity_main.xml
+     * file:
+     * Public Access, A void return value, A View as the only arg
+     * (It is the View object that was clicked)*/
+    public void sendMessage (View view) {
+        Intent intent = new Intent(this, MatchPreferencesActivity.class);
+        // EditText editText = (EditText) findViewById(R.id.editText);
+        // String message = editText.getText().toString();
+        // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
--- MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MatchPreferencesActivity.java
+++ MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MatchPreferencesActivity.java
@@ -1,93 +1,23 @@
 package com.matchmaker.matchmaker;
 
-
-import android.annotation.TargetApi;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.support.v7.app.ActionBar;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.MenuItem;
+import android.os.Bundle;
+import android.widget.TextView;
 
-import java.util.List;
-import java.util.prefs.Preferences;
-
-/**
- * A {@link PreferenceActivity} that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- * <p>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
- */
 public class MatchPreferencesActivity extends AppCompatActivity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Add 'general' preferences, defined in the XML file
+        setContentView(R.layout.activity_match_preferences);
 
-        // Display the fragment as the main content
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .replace(android.R.id.content, new MatchPreferencesFragment())
-                    .commit();
-        }
-    }
-
-    public static class MatchPreferencesFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.pref_general);
-
-            // For all preferences, attach an OpenPreferenceChangeListener so the UI summary can
-            // be updated when the preference changes
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_sport_key)));
-        }
-
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object value) {
-            String stringValue = value.toString();
-
-            if(preference instanceof ListPreference) {
-                // For list preferences, loop up the correct display value in
-                // the preferences 'entries' list (since they have separate labels/values).
-                ListPreference listPreference = (ListPreference) preference;
-                int prefIndex = listPreference.findIndexOfValue(stringValue);
-                if(prefIndex >= 0) {
-                    preference.setSummary(listPreference.getEntries()[prefIndex]);
-                }
-            } else {
-                // For other prefs, set the summary to the value's simple string representation.
-                preference.setSummary(stringValue);
-            }
-            return true;
-        }
-
-        private void bindPreferenceSummaryToValue(Preference preference) {
-            // set the listener to watch for value changes
-            preference.setOnPreferenceChangeListener(this);
-            // Trigger the listener immediately with the preference's current value
-            onPreferenceChange(preference,
-                    PreferenceManager
-                            .getDefaultSharedPreferences(preference.getContext())
-                            .getString(preference.getKey(), ""));
-        }
+        // Get the Intent that start this activity and extract the string
+        // There's no string!? Just a button
+        Intent intent = getIntent();
+        String message = "Select Your Match Preferences:";
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
     }
 }

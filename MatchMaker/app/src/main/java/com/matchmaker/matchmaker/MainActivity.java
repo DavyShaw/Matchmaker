7a3144d30a2a8388e4ddf18da802e40b3d2783ca
PamelaKelly <pamelakelly7@gmail.com>
2017-11-07 18:21:27 +0000

    time selection & update working with time dialog fragment

--- MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MainActivity.java
+++ MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MainActivity.java
@@ -7,11 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
     // public static final String EXTRA_MESSAGE = "com.matchmaker.MESSAGE";
+
+    TextView time;
+    TimePicker simpleTimePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MatchPreferencesActivity.java
+++ MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MatchPreferencesActivity.java
@@ -1,28 +1,43 @@
 package com.matchmaker.matchmaker;
 
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MatchPreferencesActivity extends AppCompatActivity {
+    private TimePicker tp1;
+    private TextView time;
+    private Calendar calendar;
+    private String format="";
+    Context ctx;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match_preferences);
         // Get the Intent that start this activity and extract the string
-        // There's no string!? Just a button
-        String message = "Select Your Match Preferences:";
         // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(message);
+        textView.setText(R.string.select_preferences_message);
 
-        
+
         //######################################SPINNER########################################
         // Setting up the Spinner for sport choice
         Spinner spinner = (Spinner) findViewById(R.id.sports_spinner);
@@ -38,7 +53,30 @@ public class MatchPreferencesActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
-
     }
 
-}
+    // reference: https://www.journaldev.com/9976/android-date-time-picker-dialog
+    public void showTimePickerDialog(View v) {
+        final EditText editText = (EditText) findViewById(R.id.time);
+        // function for displaying the time picker
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        System.out.println("IN the onTimeSet function!");
+                        // Is this the line that's breaking!?
+                        editText.setText(hourOfDay + ":" + minute);
+                        System.out.println("PROB NOT");
+                    }
+                }, mHour, mMinute, false);
+        timePickerDialog.show();
+    }
+    }
+
+
+

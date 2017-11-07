eb56d0c196e7c04e2d1d4b1e6ed8d697ad9e1346
PamelaKelly <pamelakelly7@gmail.com>
2017-11-07 12:55:22 +0000

    sports/activity dropdown on match preferences working using spinner

--- MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MainActivity.java
+++ MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MainActivity.java
@@ -1,6 +1,8 @@
 package com.matchmaker.matchmaker;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
--- MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MatchPreferencesActivity.java
+++ MatchMaker/app/src/main/java/com/matchmaker/matchmaker/MatchPreferencesActivity.java
@@ -3,6 +3,10 @@ package com.matchmaker.matchmaker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MatchPreferencesActivity extends AppCompatActivity {
@@ -11,13 +15,30 @@ public class MatchPreferencesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match_preferences);
-
         // Get the Intent that start this activity and extract the string
         // There's no string!? Just a button
-        Intent intent = getIntent();
         String message = "Select Your Match Preferences:";
         // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(message);
+
+        
+        //######################################SPINNER########################################
+        // Setting up the Spinner for sport choice
+        Spinner spinner = (Spinner) findViewById(R.id.sports_spinner);
+
+        //Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                .createFromResource(this, R.array.sports_list_values,
+                android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
     }
+
 }
